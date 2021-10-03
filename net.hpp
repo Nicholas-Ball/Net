26c9b2effe3a2196d28bd09f53298e7028dc007c
Nicholas-Ball <nicholasball085@gmail.com>
2021-10-03 10:55:07 -0500

    Added post functionality + Status Codes

--- net.hpp
+++ net.hpp
@@ -5,37 +5,80 @@
 
 class net{
     private:
+        //resposne of curl
         std::string Data;
+
+        //status code
+        std::string Code;
     public:
         //Get request
         void Get(std::string url)
         {
             //get url
-            const std::string command = "curl -G -s "+url+" -o temp.tmp";
+            const std::string command = "curl -G -s -o temp.tmp -w \"%{http_code}\n\" "+url+" > temp2.tmp";
             system(command.c_str());
 
             std::ifstream file("temp.tmp");
+            std::ifstream status("temp2.tmp");
+
+            //get status code
+            getline(status,this->Code);
 
             this->Data = "";
             std::string line;
 
-            //add data to self
+            //add response to data var
             while(getline(file,line))
             {
                 this->Data += line;
             }
 
-            //close file and delete it
+            //close files and delete them
             file.close();
+            status.close();
             remove("temp.tmp");
+            remove("temp2.tmp");
         }
 
         //Post request
-        void Post(std::string url, std::string data);
+        void Post(std::string url, std::string data)
+        {
+            //Post url
+            const std::string command = "curl -d "+data+" -s -o temp.tmp -w \"%{http_code}\n\" "+url+" > temp2.tmp";
+            system(command.c_str());
+
+            std::ifstream file("temp.tmp");
+            std::ifstream status("temp2.tmp");
+
+            //get status code
+            getline(status,this->Code);
+
+            this->Data = "";
+            std::string line;
+
+            //add response to data var
+            while(getline(file,line))
+            {
+                this->Data += line;
+            }
+
+            //close files and delete them
+            file.close();
+            status.close();
+            remove("temp.tmp");
+            remove("temp2.tmp");
+
+        }
         
-        //data accsessor
-        std::string GetData()
+        //Response accsessor
+        std::string Response()
         {
             return this->Data;
+        }
+
+        //Status Code accsessor
+        std::string StatusCode()
+        {
+            return this->Code;
         }
 };
